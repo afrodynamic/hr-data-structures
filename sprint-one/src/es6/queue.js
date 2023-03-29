f110315e7afe2b534cab233cd4f164cb2c4df8ff
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-03-29 12:28:02 -0400

    feat: complete es6 instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,29 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var key = this.size();
+
+    this.storage[key] = value;
+  }
+
+  dequeue() {
+    var firstValue = this.storage[0];
+
+    delete this.storage[0];
+
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+
+      delete this.storage[key];
+    }
+
+    return firstValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
