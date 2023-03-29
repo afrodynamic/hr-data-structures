9ab5a90a8bc8d47c2f8a19237f105154594cf82c
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-03-29 17:57:04 -0400

    feat: complete binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,67 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+
+  tree.value = value;
+
+  tree.left = null;
+  tree.right = null;
+
+  _.extend(tree, binarySearchTreeMethods);
+
+  return tree;
 };
 
+var binarySearchTreeMethods = {
+  insert: function(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  },
+
+  contains: function(value) {
+    if (value === this.value) {
+      return true;
+    } else if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    }
+  },
+
+  depthFirstLog: function(callback) {
+    callback(this.value);
+
+    if (this.left !== null) {
+      this.left.depthFirstLog(callback);
+    }
+
+    if (this.right !== null) {
+      this.right.depthFirstLog(callback);
+    }
+  },
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// insert: O(log n)
+// contains: O(log n)
+// depthFirstLog: O(n)
