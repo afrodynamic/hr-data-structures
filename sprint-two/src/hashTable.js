d9892ef4f979b961927408bf5b523dd8844646e0
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-03-29 17:55:22 -0400

    feat: complete hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,65 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (bucket === undefined) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+
+  var found = false;
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      found = true;
+    }
+  }
+
+  if (!found) {
+    bucket.push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (bucket === undefined) {
+    return undefined;
+  }
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (bucket === undefined) {
+    return undefined;
+  }
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+
+  return undefined;
 };
 
 
@@ -22,5 +71,8 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// insert: O(n) worst case, O(1) best case,
+// retrieve: O(n) worst case,  O(1) best case
+// remove: O(n) worst case, O(1) best case
 
 
