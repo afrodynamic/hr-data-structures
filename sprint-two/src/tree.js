de093107d5c4761686e872c9e8160dee1171d5e7
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-03-29 17:50:47 -0400

    feat: complete tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value) {
   var newTree = {};
+
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = null;
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+
+  if (this.children === null) {
+    this.children = [];
+  }
+
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+
+  var search = function (node) {
+    if (node.value === target) {
+      result = true;
+    } else if (node.children !== null) {
+      for (var i = 0; i < node.children.length; i++) {
+        search(node.children[i]);
+      }
+    }
+  };
+
+  search(this);
+
+  return result;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// addChild: O(1)
+// contains: O(n) worst case, O(1) best case
